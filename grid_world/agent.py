dfc31961a70aca57ea46f5507260cf15d2a0b484
Sangho.Nam <namdori61@gmail.com>
2020-11-30 11:18:14 +0900

    Env., Agent: Imp upgrade grid world.

--- grid_world/agent.py
+++ grid_world/agent.py
@@ -1,4 +1,6 @@
+from typing import Tuple, List
 import random
+import numpy as np
 
 
 class Agent:
@@ -16,3 +18,47 @@ class Agent:
         else:
             action = 3
         return action
+
+class QAgent:
+    def __init__(self) -> None:
+        self.q_table = np.zeros((5, 7, 4)) # q values
+        self.eps = 0.9
+        self.alpha = 0.01
+
+    def select_action(self, s: Tuple = None) -> int:
+        #action by eps-greedy
+        x, y = s
+        coin = random.random()
+        if coin < self.eps:
+            action = random.randint(0, 3)
+        else:
+            action_val = self.q_table[x, y, :]
+            action = np.argmax(action_val)
+        return action
+
+    def update_table(self,
+                     history: List[Tuple] = None) -> None:
+        #update q table value by episode(history)
+        cum_reward = 0
+        for transition in history[::-1]:
+            s, a, r, s_prime = transition
+            x, y = s
+            #update by MC
+            self.q_table[x, y, a] = self.q_table[x, y, a] + self.alpha * (cum_reward - self.q_table[x, y, a])
+            cum_reward = cum_reward + r
+
+    def anneal_eps(self) -> None:
+        self.eps -= 0.03
+        self.eps = max(self.eps, 0.1)
+
+    def show_table(self):
+        #Show actions that has max q value
+        q_lst = self.q_table.tolist()
+        data = np.zeros((5, 7))
+        for row_idx in range(len(q_lst)):
+            row = q_lst[row_idx]
+            for col_idx in range(len(row)):
+                col = row[col_idx]
+                action = np.argmax(col)
+                data[row_idx, col_idx] = action
+        print(data)
--- grid_world/env.py
+++ grid_world/env.py
@@ -53,3 +53,70 @@ class GridWorld:
         self.x = 0
         self.y = 0
         return self.x, self.y
+
+
+class UpgradedGridWorld:
+    def __init__(self) -> None:
+        self.x = 0
+        self.y = 0
+
+    def step(self, a: int = None) -> Tuple[Tuple[int, int], int, bool]:
+        if a == 0:
+            self.move_left()
+        elif a ==1:
+            self.move_up()
+        elif a == 2:
+            self.move_right()
+        elif a == 3:
+            self.move_down()
+
+        reward = -1
+        done = self.is_done()
+        return (self.x, self.y), reward, done
+
+    def move_left(self) -> None:
+        if self.y == 0:
+            pass
+        elif self.y == 3 and self.x in [0, 1, 2]:
+            pass
+        elif self.y == 5 and self.x in [2, 3, 4]:
+            pass
+        else:
+            self.y -= 1
+
+    def move_right(self) -> None:
+        if self.y == 1 and self.x in [0, 1, 2]:
+            pass
+        elif self.y == 3 and self.x in [2, 3, 4]:
+            pass
+        elif self.y == 6:
+            pass
+        else:
+            self.y += 1
+
+    def move_up(self) -> None:
+        if self.x == 0:
+            pass
+        elif self.x == 3 and self. y == 2:
+            pass
+        else:
+            self.x -= 1
+
+    def move_down(self) -> None:
+        if self.x == 4:
+            pass
+        elif self.x == 1 and self.y == 4:
+            pass
+        else:
+            self.x += 1
+
+    def is_done(self) -> bool:
+        if self.x == 4 and self.y == 6:
+            return True
+        else:
+            return False
+
+    def reset(self) -> Tuple[int, int]:
+        self.x = 0
+        self.y = 0
+        return (self.x, self.y)
